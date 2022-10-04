42ae8e6db3497b0344525a059c35f15f5d8fb95e
LFernandod99 <luizfernando.duartevidal1@gmail.com>
2022-10-04 09:26:50 -0300

    primeiro-exercicio-de-sql

---
+++ exercicio2.sql
@@ -0,0 +1,27 @@
+/*drop table tb_produtos;*/
+/*create database db_site;*/
+use db_site;
+/*create table tb_produtos(
+id bigint auto_increment,
+nome varchar(20),
+marca varchar (20),
+tamanho varchar(6),
+preco decimal(10,2),
+ativo boolean,
+
+primary key(id)
+);*/
+
+/*insert into tb_produtos (nome, marca, tamanho,preco, ativo) values ("Blusa", "Puma", "GG", 100.20, true);
+insert into tb_produtos (nome, marca, tamanho, preco, ativo) values ("Camisa", "Puma", "M", 209.30, true);
+insert into tb_produtos (nome, marca, tamanho, preco, ativo) values ("Tênis", "nike", "43",700, true);
+insert into tb_produtos (nome, marca, tamanho, preco, ativo) values ("short", "nike", "G", 79, true);
+insert into tb_produtos (nome, marca, tamanho, preco, ativo) values ("testeira", "nike", "G", 20, true);
+insert into tb_produtos (nome, marca, tamanho, preco, ativo) values ("MEIÃO", "ADIDAS", "G", 60, true);
+insert into tb_produtos (nome, marca, tamanho, preco, ativo) values ("CHUTEIRA", "PuUmbroma", "G", 1000, true);
+insert into tb_produtos (nome, marca, tamanho, preco, ativo) values ("Caneleira", "adidas", "G", 50, true);*/
+/*select * from tb_produtos where preco >= 500;*/
+/*select * from tb_produtos where preco <= 500;*/
+/*select * from tb_produtos*/
+
+
---
+++ exercicio3.sql
@@ -0,0 +1,25 @@
+/*create database db_escola;*/
+ use db_escola;
+ 
+ /*create table tb_alunos(
+ id bigint(5) auto_increment,
+ nome varchar (50),
+ sexo varchar (1),
+ serie int(10),
+ nota decimal(5),
+ primary key(id)
+ );*/
+ 
+ /*insert into tb_alunos (nome, sexo, serie, nota) values ("Luiz Fernando", "M", 9, 10);
+ insert into tb_alunos (nome, sexo, serie, nota) values ("Carol", "F", 9, 10);
+ insert into tb_alunos (nome, sexo, serie, nota) values ("Cadu", "M", 5, 8);
+ insert into tb_alunos (nome, sexo, serie, nota) values ("kaio", "M", 6, 7);
+ insert into tb_alunos (nome, sexo, serie, nota) values ("Delfino", "M", 9, 5);
+ insert into tb_alunos (nome, sexo, serie, nota) values ("Kevinzito", "M", 9, 6);
+ insert into tb_alunos (nome, sexo, serie, nota) values ("Amanda", "F", 5, 6);
+ insert into tb_alunos (nome, sexo, serie, nota) values ("Mano w", "M", 9, 3);*/
+ /*select * from tb_alunos;*/
+ 
+ /*select * from tb_alunos where nota >= 7;*/
+ /*select * from tb_alunos where nota <= 7;*/
+ 
